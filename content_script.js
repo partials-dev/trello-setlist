040b4d5ee1230cd411ba0259d80aed4140bf9b75
Ian Edwards <denkyuu@gmail.com>
2016-05-15 23:49:34 -0400

    Board Clipped

---
+++ content_script.js
@@ -0,0 +1,40 @@
+// Detect which list to reference
+var lastButtonClicked = null;
+
+function storeLastClicked() {
+  lastButtonClicked = $(this);
+}
+
+function addLastButtonListener() {
+  $('.js-open-list-menu').unbind('click').click(storeLastClicked);
+}
+
+
+// Add Options to Menu
+function addLi() {  
+  $($('.pop-over-content .pop-over-list')[0]).append("<hr><li><a id='copy-to-clipboard'>Copy To Clipboard</a></li>");
+  $('#copy-to-clipboard').unbind('click').click(copyToClipboard);
+}
+
+// Option Functionality
+
+function copyToClipboard() {
+  myRegEx = /"([^"]*)"/g;
+  var listItems = lastButtonClicked.closest('.list').find('.list-cards .list-card').find('.list-card-title').contents().filter(function(){ 
+    var filteredList = this.nodeType == 3; 
+    for (var i = 0; i < filteredList.length; i++) { 
+      console.log(filteredList[i].text());
+    };
+  })
+  
+  console.log(listItems[1]);
+  //console.log(myRegEx.exec(listItems));
+}
+
+//.list
+//.list-cards
+//  .list-card
+
+$(document).arrive(".js-open-list-menu", addLastButtonListener);
+$(document).arrive(".pop-over-content", addLi);
+
