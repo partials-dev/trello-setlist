c7bc9f975e3ebbca1a1e72e8774feadaf33839c0
Ian Edwards <denkyuu@gmail.com>
2016-05-17 21:41:35 -0400

    Add first pass at getting labels from cards.

--- content_script.js
+++ content_script.js
@@ -9,42 +9,61 @@ function addLastButtonListener() {
   $('.js-open-list-menu').unbind('click').click(storeLastClicked);
 }
 
-
 // Add Options to Menu
-function addLi($header) {  
-  $header.append(
-    "<hr><li><a id='copy-to-clipboard'>Copy To Clipboard</a></li> <li><a id='share'>Share To... (WIP)</a></li>"
+
+function addLi($list) {  
+  $list.append(
+    "<hr>"
+    + "<li><a id='copy-to-clipboard'>Copy To Clipboard</a></li>" 
+    + "<li><a id='share'>Share To... (WIP)</a></li>"
   );
 
   $('#copy-to-clipboard').unbind('click').click(copyToClipboard);
   $('#share').unbind('click').click(share);
 }
 
-// Notifications
-
-Notification.requestPermission().then(function(result) {
-  console.log(result);
-});
-
-function spawnNotification(theBody, theIcon, theTitle) {
-  var options = {
-        body: theBody,
-        icon: theIcon
-    }
-  var n = new Notification(theTitle,options);
-}
-
-
 // Option Functionality
 
 function getPlainText() {
-  var listItems = lastButtonClicked.closest('.list').find('.list-cards .list-card').find('.list-card-title').contents();
-  var filteredList = listItems.filter(function () { return this.nodeType === 3; });
-  
+  var cards = lastButtonClicked.closest('.list').find('.list-cards .list-card')
   var plainText = "";
-  for (var i = 0; i < filteredList.length; i++) {
-    plainText += filteredList[i].data + "\n"
+  for (var i = 0; i < cards.length; i++) {
+    var titleObject = $(cards[i]).find('.list-card-title').contents().filter(function () { return this.nodeType === 3; })[0];
+    if (!titleObject) {break;}
+
+    var title = titleObject.data
+
+    if ($(cards[i]).find('.card-label').text() === '\xa0') {
+
+      // Implementation 1: slice
+      //var color = $(cards[i]).find('.card-label').attr('class').split('-')[3].slice(0,1);
+      //var label = " [" + color.toUpperCase() +"]";
+
+      // Implementation 2: regex capture group
+      var getColorRegEx = /card-label-([a-z]+?)/g;
+      var regExColor = getColorRegEx.exec($(cards[i]).find('.card-label').attr('class'));
+      var label = "RegEx: [" + regExColor[1].toUpperCase() + "]";
+
+      // Implementation 3: color cache
+      //var labelColors = ['green', 'yellow', 'orange', 'purple', 'sky', 'lime', 'pink', 'black', 'null'];
+      //var labelClass = $(cards[i]).find('.card-label').attr('class');
+      //var label = labelColors.filter(function (color) {
+        //var matchColor = new RegExp(color);
+        //return matchColor.test(labelClass);
+      //}).reduce(function (label, color) {
+        //label += " [" + color[0] + "]"
+        //return label;
+      //}, "");
+
+    } else if ($(cards[i]).find('.card-label')[0]) {
+      var label = " [" + $(cards[i]).find('.card-label').text() + "]";
+    } else {
+      var label = "";
+    }
+
+    plainText += title + label + "\n";
   }
+  console.log(plainText);
   return plainText;
 }
 
@@ -56,12 +75,30 @@ function copyToClipboard () {
   document.execCommand('SelectAll');
   document.execCommand('Copy');
   document.body.removeChild(copyFrom);
-  spawnNotification('List copied to clipboard!', null, 'Trello Setlist Utility');
+  console.log('Copied to Clipboard');
+  spawnNotification('List copied to clipboard!', './icon.png', 'Trello Setlist Utility');
 } 
 
 function share() {
   console.log('Share Button Clicked');
+  spawnNotification('Sorry, not implemented yet.', './icon.png', 'Trello Setlist Utility');
 }
+
+// Notifications
+
+function spawnNotification(theBody, theIcon, theTitle) {
+  var options = {
+        body: theBody,
+        icon: theIcon
+    }
+  var n = new Notification(theTitle,options);
+}
+
+//Initialization
+
+Notification.requestPermission().then(function(result) {
+  console.log("TSU Notification permission: " + result);
+});
 
 $(document).arrive(".js-open-list-menu", addLastButtonListener);
 $(document).arrive('.pop-over-header', function () {
@@ -73,4 +110,3 @@ $(document).arrive('.pop-over-header', function () {
     }
   }, 50)
 });
-
